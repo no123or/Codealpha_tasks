ca0b6493aefc90c110b0e77caf137b0abe664438
noor ul iman <nooriman753@gmail.com>
2024-10-31 17:47:08 +0500

    Calculator web app files

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
-console.log("HERE COMES THE AGE CALCULATOR");
+// console.log("Hello world");
 
-function btnSubmit() {
-    var d1 = parseInt(document.getElementById('date').value);
-    var m1 = parseInt(document.getElementById('month').value);
-    var y1 = parseInt(document.getElementById('year').value);
-
-    var date = new Date();
-    var d2 = date.getDate();
-    var m2 = 1 + date.getMonth();
-    var y2 = date.getFullYear();
-    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    // Leap year adjustment
-    if ((y1 % 4 === 0 && y1 % 100 !== 0) || (y1 % 400 === 0)) {
-        month[1] = 29;
-    }
-
-    if (d1 > d2) {
-        d2 = d2 + month[m2 - 1];
-        m2 = m2 - 1;
-    }
-    if (m1 > m2) {
-        m2 = m2 + 12;
-        y2 = y2 - 1;
-    }
-    var d = d2 - d1;
-    var m = m2 - m1;
-    var y = y2 - y1;
-
-    document.getElementById('text').innerHTML = "Your age is " + y + " years, " + m + " months, and " + d + " days.";
-}
-
-
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click' , (e)=>{
+        if (e.target.innerHTML == '=') {
+            string = eval(string)
+            document.querySelector('input').value = string;
+        } else if(e.target.innerHTML == 'AC') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C'
+        ){
+            string = string.slice(0,-1);
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target)
+string = string + e.target.innerHTML;
+document.querySelector('input').value = string;
+        }
+    })
+})
